f313c6d07fecbc2a40014641b676d501656165b3
huynhdev24 <huynh.dev.24@gmail.com>
2023-04-20 16:16:02 +0700

    refactor hero form html

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -11,7 +11,9 @@ export class HeroFormComponent {
   powers = ['Really Smart', 'Super Flexible', 'Super Hot', 'Weather Changer'];
   model = new Hero(18, 'Dr. IQ', this.powers[0], 'Chuck Overstreet');
   submitted = false;
-  onSubmit() { this.submitted = true; }
+  onSubmit() { 
+    this.submitted = true; 
+  }
 
   newHero() {
     this.model = new Hero(42, '', '');
